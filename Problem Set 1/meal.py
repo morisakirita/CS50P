fb50c2a2d8ac2f6e7416939c124f38b9df048b7e
Gabriel Augusto Morisaki Rita <morisakirita+github@proton.me>
2024-09-25 13:54:40 -0300

    Added support for 12-hour times

--- Problem Set 1/meal.py
+++ Problem Set 1/meal.py
@@ -11,8 +11,15 @@ def main():
 
 def convert(time):
     time = time.split(":")
-    converted_time = float(time[0]) + float(time[-1])/60
-    return converted_time
+    hour = float(time[0])
+    time = time[-1].split(" ")
+    converted_time = hour + float(time[0])/60
+    if "a.m." in time[-1]:
+        return converted_time
+    elif "p.m." in time[-1]:
+        return converted_time + 12
+    else:
+        return converted_time
 
 
 if __name__ == "__main__":
