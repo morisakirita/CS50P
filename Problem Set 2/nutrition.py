bca0110b5ff0df221828b98408a2701ff355149b
Gabriel Augusto Morisaki Rita <morisakirita+github@proton.me>
2024-09-29 18:28:11 -0300

    Create nutrition.py

---
+++ Problem Set 2/nutrition.py
@@ -0,0 +1,36 @@
+def main():
+    item = input("Item: ").lower().strip()
+    fruit_nutrition(item)
+
+
+def fruit_nutrition(item):
+    fruits = {
+        "apple" : "130",
+        "avocado" : "50",
+        "banana" : "110",
+        "cantaloupe": "50",
+        "grapefruit" : "60",
+        "grapes": "90",
+        "honeydew melon" : "50",
+        "kiwifruit" : "90",
+        "lemon" : "15",
+        "lime" : "20",
+        "nectarine" : "60",
+        "orange" : "80",
+        "peach" : "80",
+        "pear" : "100",
+        "pineapple" : "50",
+        "plums" : "70",
+        "stawberries" : "50",
+        "sweet cherries" : "100",
+        "tagerine" : "50",
+        "watermelon" : "80",
+    }
+
+    for fruit in fruits:
+        if fruit == item:
+            print(fruits[fruit])
+
+
+if __name__ == "__main__":
+    main()
